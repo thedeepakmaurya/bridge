e2680809cdf457772d74af2eb3bd30f6df354511
thedeepakmaurya <deipkamaurya@gmail.com>
2025-09-29 13:35:10 +0530

    feat(home): community section

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -62,6 +62,54 @@ document.querySelectorAll(".community-swiper").forEach((swiperEl) => {
   });
 });
 
+document.querySelectorAll(".team-swiper").forEach((swiperEl) => {
+  new Swiper(swiperEl, {
+    loop: false,
+
+    spaceBetween: 16,
+
+    slidesPerView: 1.1,
+
+    navigation: {
+      nextEl: swiperEl.parentElement.querySelector(".team-button-next"),
+
+      prevEl: swiperEl.parentElement.querySelector(".team-button-prev"),
+    },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 1.1,
+        spaceBetween: 16,
+      },
+
+      640: {
+        slidesPerView: 2,
+
+        spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 2.5,
+
+        spaceBetween: 20,
+      },
+
+      1024: {
+        slidesPerView: 4,
+
+        spaceBetween: 24,
+      },
+
+      1280: {
+        slidesPerView: 4,
+
+        spaceBetween: 24,
+      },
+    },
+  });
+});
+
+
 document.querySelectorAll(".swiper").forEach((swiperEl) => {
   new Swiper(swiperEl, {
     loop: false,
